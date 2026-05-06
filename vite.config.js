85d99a8c2869a59b4036b0d1153d89874a185be0
vinuthas0102 <vinuthas0102@gmail.com>
2026-05-06 23:04:38 +0530

    Update dev server heap & esbuild target

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    target: 'es2020',
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
