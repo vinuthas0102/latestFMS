2e7aa7fa6827db12df29f08fb65b334b95a626d7
vinuthas0102 <vinuthas0102@gmail.com>
2026-05-26 14:08:45 +0530

    Updated QuarterRequestsPage.tsx

--- src/pages/QuarterRequestsPage.tsx
+++ src/pages/QuarterRequestsPage.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useState } from 'react';
+import React, { Suspense, useState, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { useNavigate } from 'react-router-dom';
 import {
