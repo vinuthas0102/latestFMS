00143c8439b7139716566b73a7199afdd74c29ec
vinuthas0102 <vinuthas0102@gmail.com>
2026-07-30 20:33:12 +0530

    Updated tsconfig.tsbuildinfo

--- README.md
+++
@@ -1,3 +0,0 @@
-# latestFMS
-
-[![Open in Bolt](https://bolt.new/static/open-in-bolt.svg)](https://bolt.new/~/sb1-eqft5vp7)
--- src/types/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './common.types';
-export * from './user.types';
-export * from './property.types';
-export * from './booking.types';
-export * from './dateBlock.types';
